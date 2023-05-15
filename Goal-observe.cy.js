cacd8316486eaf24a365512a388fe6b4b70c8678
Jana-mousa <92536722+Jana-mousa@users.noreply.github.com>
2023-05-15 09:45:58 -0700

    Update Goal-observe.cy.js

--- Goal-observe.cy.js
+++ Goal-observe.cy.js
@@ -1,117 +1,137 @@
 describe('observe', () => {
-  it('1-goals-page-All-Goals,Group-filter-all,Level-filters-all', function () {
+    beforeEach(()=>{  
     cy.visit('https://goal-dev.mdx.ac.uk');
     cy.get('#id_username').type('JanaMousa');
     cy.get('#id_password').type('jana@1@0123');
     cy.get(':nth-child(4) > div > .form-control').select('staff');
     cy.get('.btn').click();
     cy.get('.fas').should('exist');
+  
     //open
     cy.get(':nth-child(2) > .row > .col-sm-4 > .nav > .nav-item > .nav-link').click();
-    //Goal page
     cy.get('.mr-auto > :nth-child(2) > .nav-link').click();
     
     //Goal Filter
     cy.get(':nth-child(1) > .custom-control-label > h5').click();
-    
-    //Group filter all
-    cy.get(
-      '[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(1)'
-    ).click();
-    //cy.get('.align-items-center > :nth-child(3) > .btn').click();
 
-    //1-Observe-topic1-with-all-group 
-    cy.get(':nth-child(1) > .custom-control-label > h5').click();
-    cy.get(':nth-child(3) > .custom-control-label > h5').click();
-    cy.get('[style="height: 10vh"] > :nth-child(1) > .custom-control-label').click();
-    cy.get('.align-items-center > :nth-child(3) > .btn').click();
-    cy.get(':nth-child(5) > .btn').click();
+    //Group filter all
+    cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(1)').click();
+
+    })
+
+    //1-Observe-topic1-with-all-group
+  it('1-Observe-topic1-with-all-group', function ()  {
+    cy.get(':nth-child(1) > .custom-control-label > h5').click();//click All Goal
+    cy.get(':nth-child(3) > .custom-control-label > h5').click();//click topic 1
+    cy.get('[style="height: 10vh"] > :nth-child(1) > .custom-control-label').click(); // click not observe
+    cy.get('.align-items-center > :nth-child(3) > .btn').click();//click Apply filter
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
+   
+
    
     //2-observe-topic1+2-with-all-group
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get('.align-items-center > :nth-child(3) > .btn').click();
-    cy.get(':nth-child(5) > .btn').click();
-
+    it('2-observe-topic1+2-with-all-group',function(){
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get('.align-items-center > :nth-child(3) > .btn').click();//click apply filter
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
     //3-observe-topic1+2+3-with-all-group
-    cy.get(':nth-child(10) > .custom-control-label > h5').click();
-    cy.get('.align-items-center > :nth-child(3) > .btn').click();
-    cy.get(':nth-child(5) > .btn').click();
+    it('3-observe-topic1+2+3-with-all-group',function(){
+    cy.get(':nth-child(10) > .custom-control-label > h5').click();//click topic 3
+    cy.get('.align-items-center > :nth-child(3) > .btn').click();//click apply filter
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
     //4-observe-topic1-without_group3
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get(':nth-child(10) > .custom-control-label > h5').click();
-    cy.get(':nth-child(5) > .custom-control > .custom-control-label').click();
-    cy.get(':nth-child(5) > .btn').click();
+    it('4-observe-topic1-without_group3',function(){
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get(':nth-child(10) > .custom-control-label > h5').click();//click topic 3
+    cy.get(':nth-child(5) > .custom-control > .custom-control-label').click();//click group 3
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
 
     //5-observe-topic2-without-group3
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get(':nth-child(3) > .custom-control-label > h5').click();
-    cy.get(':nth-child(5) > .btn').click();
+    it('5-observe-topic2-without-group3',function(){
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get(':nth-child(3) > .custom-control-label > h5').click();//click topic 1
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
     //6-observe-topic3-without-group3-and-qa
-    cy.get(':nth-child(10) > .custom-control-label > h5').click();
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(6) > .custom-control > .custom-control-label').click();
-    cy.get(':nth-child(5) > .btn').click();
-
+    it('6-observe-topic3-without-group3-and-qa',function(){
+    cy.get(':nth-child(10) > .custom-control-label > h5').click();//click topic 3
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(6) > .custom-control > .custom-control-label').click();//click group qa
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
 
     //7-observe-topic1+2-without-group3-and-qa
-    cy.get(':nth-child(10) > .custom-control-label > h5').click();
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get(':nth-child(3) > .custom-control-label > h5').click();
-    cy.get(':nth-child(5) > .btn').click();
-
+    it('7-observe-topic1+2-without-group3-and-qa',function(){
+    cy.get(':nth-child(10) > .custom-control-label > h5').click();//click topic 3
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get(':nth-child(3) > .custom-control-label > h5').click();//click topic 1
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
     //8-observe-topic1+3-without-group3-and-qa
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get(':nth-child(10) > .custom-control-label > h5').click();
-    cy.get(':nth-child(5) > .btn').click();
-
+    it('8-observe-topic1+3-without-group3-and-qa',function(){
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get(':nth-child(10) > .custom-control-label > h5').click();//click  topic 3
+    cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
     //9-observe-topic2+3-without-group3-and-qa
-    cy.get(':nth-child(3) > .custom-control-label > h5').click();
-    cy.get(':nth-child(7) > .custom-control-label > h5').click();
-    cy.get(':nth-child(5) > .btn').click();
-
+    it('9-observe-topic2+3-without-group3-and-qa',function(){
+    cy.get(':nth-child(3) > .custom-control-label > h5').click();//click topic 1
+    cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+    cy.get(':nth-child(5) > .btn').click();//click observe 
+  })
 
    //10-observe-topic3-without-group3
-   cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(6) > .custom-control > .custom-control-label').click();
-   cy.get(':nth-child(7) > .custom-control-label > h5').click();
-   cy.get(':nth-child(5) > .btn').click();
+   it('10-observe-topic3-without-group3',function(){
+   cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(6) > .custom-control > .custom-control-label').click();//click group qa
+   cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+   cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
    //11-observe-topic1+2+3-without-group2+3
-   cy.get(':nth-child(7) > .custom-control-label > h5').click();
-   cy.get(':nth-child(3) > .custom-control-label > h5').click();
-   cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(4) > .custom-control > .custom-control-label').click();
-   cy.get(':nth-child(5) > .btn').click();
-
+   it('11-observe-topic1+2+3-without-group2+3',function(){
+   cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+   cy.get(':nth-child(3) > .custom-control-label > h5').click();//click topic 1
+   cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(4) > .custom-control > .custom-control-label').click();//click group 2
+   cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
    //12-observe-topic1+2+3-without-group2+3+qa
-   cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(6) > .custom-control > .custom-control-label').click();
-   cy.get(':nth-child(5) > .btn').click();
- 
+   it('12-observe-topic1+2+3-without-group2+3+qa',function(){
+   cy.get('[style="overflow-y: scroll;height: 30.7vh"] > :nth-child(6) > .custom-control > .custom-control-label').click();//click group qa
+   cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
    //13-observe-topic1-without-group2+3+qa
-   cy.get(':nth-child(10) > .custom-control-label > h5').click();
-   cy.get(':nth-child(7) > .custom-control-label > h5').click();
-   cy.get(':nth-child(5) > .btn').click();
+   it('13-observe-topic1-without-group2+3+qa',function(){
+   cy.get(':nth-child(10) > .custom-control-label > h5').click();//click topic 3
+   cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic 2
+   cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
    //14-observe-topic1+2-without-group2+3+qa
-   cy.get(':nth-child(7) > .custom-control-label > h5').click();
-   cy.get(':nth-child(5) > .btn').click();
+   it('14-observe-topic1+2-without-group2+3+qa',function(){
+   cy.get(':nth-child(7) > .custom-control-label > h5').click();//click topic2
+   cy.get(':nth-child(5) > .btn').click();//click observe
+  })
 
    //15-observe-topic2+3-without-group2+3+qa
-   cy.get(':nth-child(10) > .custom-control-label > h5').click();
-   cy.get(':nth-child(5) > .btn').click();
+   it('15-observe-topic2+3-without-group2+3+qa',function(){
+   cy.get(':nth-child(10) > .custom-control-label > h5').click();//click topic 3
+   cy.get(':nth-child(5) > .btn').click();//click observe 
+  })
 
-
-
-    
-    
-  });});
+  });
 
 
 
